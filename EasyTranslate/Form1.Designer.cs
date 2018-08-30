d338c65463b44e85a0c35555963565dbf7f1907e
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2018-08-30 19:38:54 +0430

    project finish

--- EasyTranslate/Form1.Designer.cs
+++ EasyTranslate/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.locationString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.Label();
+            this.AddToTextClick = new System.Windows.Forms.Button();
+            this.changesText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(337, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get String";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnGetStringClick);
+            // 
+            // locationString
+            // 
+            this.locationString.Location = new System.Drawing.Point(411, 41);
+            this.locationString.Name = "locationString";
+            this.locationString.Size = new System.Drawing.Size(661, 44);
+            this.locationString.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Your text file location";
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.ForeColor = System.Drawing.Color.Coral;
+            this.error.Location = new System.Drawing.Point(328, 117);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(434, 37);
+            this.error.TabIndex = 3;
+            this.error.Text = "Please enter your file location";
+            this.error.Visible = false;
+            // 
+            // AddToTextClick
+            // 
+            this.AddToTextClick.Location = new System.Drawing.Point(357, 443);
+            this.AddToTextClick.Name = "AddToTextClick";
+            this.AddToTextClick.Size = new System.Drawing.Size(337, 102);
+            this.AddToTextClick.TabIndex = 4;
+            this.AddToTextClick.Text = "Add to Text";
+            this.AddToTextClick.UseVisualStyleBackColor = true;
+            this.AddToTextClick.Click += new System.EventHandler(this.AddToText_Click);
+            // 
+            // changesText
+            // 
+            this.changesText.Location = new System.Drawing.Point(411, 352);
+            this.changesText.Name = "changesText";
+            this.changesText.Size = new System.Drawing.Size(661, 44);
+            this.changesText.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 359);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(335, 37);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Chnages text Location";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1131, 619);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.changesText);
+            this.Controls.Add(this.AddToTextClick);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.locationString);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Easy Trranslate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox locationString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label error;
+        private System.Windows.Forms.Button AddToTextClick;
+        private System.Windows.Forms.TextBox changesText;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- EasyTranslate/Form1.cs
+++ EasyTranslate/Form1.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,65 @@ namespace EasyTranslate
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGetStringClick(object sender, EventArgs e)
+        {
+            if (locationString.Text != null)
+            {
+                var list = new List<string>();
+
+                var logFile = File.ReadAllLines(@locationString.Text.Replace(@"\\", @"\"));
+
+                foreach (var str in logFile)
+                {
+                    var mcol = Regex.Matches(str, @">\b(.+?)\b<");
+
+                    foreach (Match m in mcol)
+                    {
+                        list.Add(m.ToString().Replace("<", "").Replace(">", ""));
+                    }
+                }
+
+                File.WriteAllLines("SavedLists.txt", list);
+
+                Process.Start("SavedLists.txt");
+            }
+            else
+            {
+                error.Visible = true;
+            }
+        }
+
+        private void AddToText_Click(object sender, EventArgs e)
+        {
+            var list = new List<string>();
+
+            var logFirstFile = File.ReadAllLines(@locationString.Text.Replace(@"\\", @"\"));
+            var logFile = File.ReadAllLines(@changesText.Text.Replace(@"\\", @"\"));
+
+            var i = 0;
+
+            foreach (var str in logFirstFile)
+            {
+                var mcol = Regex.Matches(str, @">\b(.+?)\b<");
+                var mcol1 = Regex.Matches(str, @"<sys\b(.+?)\b"">");
+
+                foreach (Match m in mcol)
+                {
+                    foreach (Match match in mcol1)
+                    {
+                        list.Add(m.ToString().Replace(m.ToString(), match + logFile[i] + @"</sys:String>"));
+
+                        i++;
+
+                    }
+                }
+            }
+
+            File.WriteAllLines("result.txt", list);
+
+            Process.Start("result.txt");
         }
     }
 }
