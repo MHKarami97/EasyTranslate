1dd5e0fd7516d11faeec6547b13e513daf791159
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2018-08-30 21:50:13 +0430

    change form icon

--- EasyTranslate/Form1.Designer.cs
+++ EasyTranslate/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.locationString = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -113,6 +114,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.locationString);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Easy Trranslate";
             this.ResumeLayout(false);
